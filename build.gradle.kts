b98cbf61d308087b086e8c770ff49321551c5dcb
CroaBeast <66433837+CroaBeast@users.noreply.github.com>
2026-08-20 23:48:01 -0500

    chore: update CyberCore to 2.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     // Spigot API
     compileOnly("org.spigotmc:spigot-api:1.16.5-R0.1-SNAPSHOT")
 
-    implementation("com.bitaspire:CyberCore:2.1.0")
+    implementation("com.bitaspire:CyberCore:2.1.1")
 
     compileOnly("me.clip:placeholderapi:2.11.6")
     compileOnly(files("libraries/RivalHarvesterHoesAPI.jar"))
